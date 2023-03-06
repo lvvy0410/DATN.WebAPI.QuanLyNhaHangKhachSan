0c5800366a8baa7e152c46b3fe61efbc8345977c
lvvy0410 <levanvy.work.0337909492@gmail.com>
2023-03-06 09:28:39 +0700

    Tai file KhachHangDAO, program.cs
    Tai file KhachHangDAO, program.cs

--- DAO/KhachHangDAO.cs
+++ DAO/KhachHangDAO.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using DTO.Context;
+using DTO.Model;
+using DTO.Public;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +12,139 @@ namespace DAO
 {
     public class KhachHangDAO
     {
+        private readonly QuanLyNhaHangKhachSanContext dbcontext;
+
+        public KhachHangDAO(QuanLyNhaHangKhachSanContext dbcontext)
+        {
+            this.dbcontext = dbcontext;
+        }
+        public async Task<ErrorMessageDTO> LayDanhSachKhachHang()
+        {
+            ErrorMessageDTO error = new ErrorMessageDTO();
+            try
+            {
+                error.data = await dbcontext.KhachHangs.ToListAsync();
+                error.flagThanhCong = true;
+                return await Task.FromResult(error);
+            }
+            catch (Exception ex)
+            {
+                error.flagBiLoiEx = true;
+                error.errorCode = Convert.ToInt32(ErrorCodeEnum.InternalServerError).ToString();
+                error.message = ex.Message;
+                return await Task.FromResult(error);
+            }
+        }
+        public async Task<ErrorMessageDTO> LayKhachHang(int khachHangID)
+        {
+            //return dbcontext.khachHangs.Where(p => p.khachHangID == khachHangID).FirstOrDefault();
+            ErrorMessageDTO error = new ErrorMessageDTO();
+            try
+            {
+                KhachHang? item = await dbcontext.KhachHangs.Where(p => p.KhachHangId == khachHangID).FirstOrDefaultAsync();
+                if (item == null)
+                {
+                    error.errorCode = Convert.ToInt32(ErrorCodeEnum.KhongTimThay).ToString();
+                    error.message = ResponseDTO.GetValueError(ErrorCodeEnum.KhongTimThay);
+                    error.flagThanhCong = false;
+                    return await Task.FromResult(error);
+                }
+
+                error.data = item;
+                error.flagThanhCong = true;
+                return await Task.FromResult(error);
+
+            }
+            catch (Exception ex)
+            {
+                error.flagBiLoiEx = true;
+                error.errorCode = Convert.ToInt32(ErrorCodeEnum.InternalServerError).ToString();
+                error.message = ex.Message;
+                return await Task.FromResult(error);
+            }
+        }
+        public async Task<ErrorMessageDTO> ThemKhachHang(KhachHang obkhachhang)
+        {
+            ErrorMessageDTO error = new ErrorMessageDTO();
+            try
+            {
+                try
+                {
+                    dbcontext.KhachHangs.Add(obkhachhang);
+                    error.data = await dbcontext.SaveChangesAsync();
+                    error.flagThanhCong = true;
+                    return await Task.FromResult(error);
+                }
+                catch
+                {
+                    error.errorCode = Convert.ToInt32(ErrorCodeEnum.KhongTheThem).ToString();
+                    error.message = ResponseDTO.GetValueError(ErrorCodeEnum.KhongTheThem);
+                    error.flagThanhCong = false;
+                    return await Task.FromResult(error);
+                }
+            }
+            catch (Exception ex)
+            {
+                error.errorCode = Convert.ToInt32(ErrorCodeEnum.InternalServerError).ToString();
+                error.message = ResponseDTO.GetValueError(ErrorCodeEnum.InternalServerError);
+                error.flagBiLoiEx = true;
+                return await Task.FromResult(error);
+            }
+        }
+        public async Task<ErrorMessageDTO> CapNhatKhachHang(KhachHang obkhachHang)
+        {
+            ErrorMessageDTO error = new ErrorMessageDTO();
+            try
+            {
+                KhachHang? obkhachHangId = dbcontext.KhachHangs.Where(p => p.KhachHangId == obkhachHang.KhachHangId).FirstOrDefault();
+                if (obkhachHangId == null)
+                {
+                    error.errorCode = Convert.ToInt32(ErrorCodeEnum.KhongTimThay).ToString();
+                    error.message = ResponseDTO.GetValueError(ErrorCodeEnum.KhongTimThay);
+                    error.flagThanhCong = false;
+                    return await Task.FromResult(error);
+                }
+                obkhachHangId.TenKhachHang = obkhachHang.TenKhachHang;
+                obkhachHangId.Sdt = obkhachHang.Sdt;
+                error.data = await dbcontext.SaveChangesAsync();
+                error.flagThanhCong = true;
+                return await Task.FromResult(error);
+            }
+            catch (Exception ex)
+            {
+                error.errorCode = Convert.ToInt32(ErrorCodeEnum.InternalServerError).ToString();
+                error.message = ResponseDTO.GetValueError(ErrorCodeEnum.InternalServerError);
+                error.flagBiLoiEx = true;
+                return await Task.FromResult(error);
+            }
+        }
+        public async Task<ErrorMessageDTO> XoaKhachHang(int KhachHangID)
+        {
+            ErrorMessageDTO error = new ErrorMessageDTO();
+            try
+            {
+                KhachHang? item = await dbcontext.KhachHangs.Where(p => p.KhachHangId == KhachHangID).FirstOrDefaultAsync();
+                if (item == null)
+                {
+                    error.errorCode = Convert.ToInt32(ErrorCodeEnum.NotFound).ToString();
+                    error.message = ResponseDTO.GetValueError(ErrorCodeEnum.KhongTimThay);
+                    error.flagThanhCong = false;
+                    return await Task.FromResult(error);
+                }
+                dbcontext.KhachHangs.Remove(item);
+                error.data = await dbcontext.SaveChangesAsync();
+                error.errorCode = Convert.ToInt32(ErrorCodeEnum.XoaThanhCong).ToString();
+                error.message = ErrorCodeEnum.XoaThanhCong.ToString();
+                error.flagThanhCong = true;
+                return await Task.FromResult(error);
+            }
+            catch (Exception ex)
+            {
+                error.flagBiLoiEx = true;
+                error.errorCode = Convert.ToInt32(ErrorCodeEnum.InternalServerError).ToString();
+                error.message = ex.Message;
+                return await Task.FromResult(error);
+            }
+        }
     }
 }
