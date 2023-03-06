00a6371ac65b6d15ef43edaf9f5361b8a09eb256
lvvy0410 <levanvy.work.0337909492@gmail.com>
2023-03-06 09:54:37 +0700

    abc
    abc

--- DATN.WebAPI.QuanLyNhaHangKhachSan/Program.cs
+++ DATN.WebAPI.QuanLyNhaHangKhachSan/Program.cs
@@ -12,29 +12,7 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<QuanLyNhaHangKhachSanContext>(opt =>
     opt.UseSqlServer(builder.Configuration.GetConnectionString("connectionString")));
-builder.Services.AddTransient<BanDAO, BanDAO>();
-builder.Services.AddTransient<DichVuDAO, DichVuDAO>();
-builder.Services.AddTransient<GoiMonDAO, GoiMonDAO>();
-builder.Services.AddTransient<HangHoaDAO, HangHoaDAO>();
-builder.Services.AddTransient<KhachHangDAO, KhachHangDAO>();
-builder.Services.AddTransient<KhoDAO, KhoDAO>();
-builder.Services.AddTransient<LichSuDoiDAO, LichSuDoiDAO>();
-builder.Services.AddTransient<LoaiBanDAO, LoaiBanDAO>();
-builder.Services.AddTransient<LoaiPhongDAO, LoaiPhongDAO>();
-builder.Services.AddTransient<LoaiPhieuDAO, LoaiPhieuDAO>();
-builder.Services.AddTransient<NguoiDungDAO, NguoiDungDAO>();
-builder.Services.AddTransient<PhieuDatBanChiTietDAO, PhieuDatBanChiTietDAO>();
-builder.Services.AddTransient<PhieuDatDAO, PhieuDatDAO>();
-builder.Services.AddTransient<PhieuDatPhongChiTietDAO, PhieuDatPhongChiTietDAO>();
-builder.Services.AddTransient<PhieuNhanBanChiTietDAO, PhieuNhanBanChiTietDAO>();
-builder.Services.AddTransient<PhieuNhanDAO, PhieuNhanDAO>();
-builder.Services.AddTransient<PhieuNhanPhongChiTietDAO, PhieuNhanPhongChiTietDAO>();
-builder.Services.AddTransient<PhieuNhapChiTietDAO, PhieuNhapChiTietDAO>();
-builder.Services.AddTransient<PhieuNhapDAO, PhieuNhapDAO>();
-builder.Services.AddTransient<PhieuThuDAO, PhieuThuDAO>();
-builder.Services.AddTransient<PhieuXuatDAO, PhieuXuatDAO>();
-builder.Services.AddTransient<TrangThaiDAO, TrangThaiDAO>();
-builder.Services.AddTransient<TrangThietBiDAO, TrangThietBiDAO>();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
