c26cef691280b34bdb56a6cb9909bd39493e17b5
lvvy0410 <levanvy.work.0337909492@gmail.com>
2023-03-30 10:21:42 +0700

    aduvjp
    aduvjp

--- DATN.WebAPI.QuanLyNhaHangKhachSan/Program.cs
+++ DATN.WebAPI.QuanLyNhaHangKhachSan/Program.cs
@@ -86,10 +86,28 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+//if (app.Environment.IsDevelopment())
+//{
+//    app.UseSwagger();
+//    app.UseSwaggerUI();
+//}
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment() || app.Environment.IsProduction() || app.Environment.IsStaging())
 {
+
     app.UseSwagger();
-    app.UseSwaggerUI();
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwaggerUI();
+    }
+    else
+    {
+        app.UseSwaggerUI(options =>
+        {
+            options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+            options.RoutePrefix = string.Empty;
+        });
+    }
 }
 app.UseHttpsRedirection();
 app.UseAuthentication();
