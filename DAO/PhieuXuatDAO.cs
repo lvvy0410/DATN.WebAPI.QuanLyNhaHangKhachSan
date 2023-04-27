ef954392885c2b64a8b61c87e5d9246d3dd934c4
thanhlen2502 <113028186+subalame11@users.noreply.github.com>
2023-04-27 08:30:22 +0700

    phieu xuat
    phieu xuat

--- DAO/PhieuXuatDAO.cs
+++ DAO/PhieuXuatDAO.cs
@@ -1,6 +1,7 @@
 ﻿using DTO.Context;
 using DTO.DieuKienLoc;
 using DTO.Public;
+using DTO.publicDTO;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -33,6 +34,34 @@ namespace DAO
                 error.flagBiLoiEx = true;
                 error.errorCode = Convert.ToInt32(ErrorCodeEnum.InternalServerError).ToString();
                 error.message = ex.Message;
+                return await Task.FromResult(error);
+            }
+        }
+        public async Task<ErrorMessageDTO> ThemDV(DichVuDTO dichvu)
+        {
+            ErrorMessageDTO error = new ErrorMessageDTO();
+            try
+            {
+
+                if (error.flagBiLoiEx)
+                {
+                    error.errorCode = Convert.ToInt32(ErrorCodeEnum.KhongTheThem).ToString();
+                    error.message = ResponseDTO.GetValueError(ErrorCodeEnum.KhongTheThem);
+                    error.flagThanhCong = false;
+                    return await Task.FromResult(error);
+                }
+                dbcontext.DichVus.Add(dichvu);
+                error.data = await dbcontext.SaveChangesAsync();
+                error.flagThanhCong = true;
+                return await Task.FromResult(error);
+
+
+            }
+            catch (Exception ex)
+            {
+                error.errorCode = Convert.ToInt32(ErrorCodeEnum.InternalServerError).ToString();
+                error.message = ResponseDTO.GetValueError(ErrorCodeEnum.InternalServerError);
+                error.flagBiLoiEx = true;
                 return await Task.FromResult(error);
             }
         }
