d8917892d4484489c84df79f7c245017491eeee8
Le Van Vy <0306201407@caothang.edu.vn>
2023-05-17 10:14:45 +0700

    thêm đặt phòng
    thêm đặt phòng

---
+++ DTO/MultiTable/NhanPhong.cs
@@ -0,0 +1,15 @@
+﻿using DTO.publicDTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO.MultiTable
+{
+    public class NhanPhong
+    {
+        public PhieuNhanDTO phieuNhanDTO { get; set; }
+        public List<PhieuNhanPhongChiTietDTO> phieuNhanPhongChiTietDTOs { get; set; }
+    }
+}
