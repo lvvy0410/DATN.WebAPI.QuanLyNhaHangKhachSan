cb34d0816ac5034ac0679b05043962ae50f8f34f
thanhlen2502 <113028186+subalame11@users.noreply.github.com>
2023-06-08 14:55:50 +0700

    Lấy bàn và loại bạn

--- DTO/publicDTO/DichVuDTO.cs
+++ DTO/publicDTO/DichVuDTO.cs
@@ -9,12 +9,13 @@ namespace DTO.publicDTO
 {
     public class DichVuDTO:DichVu
     {
+        public string? TrangThai { get; set; }
         public virtual Ban? Ban { get; set; }
         public virtual HangHoa? HangHoa { get; set; } 
         public virtual PhieuNhan? PhieuNhan { get; set; }
         public virtual Phong? Phong { get; set; }
 
         public string? GhiChu { get; set; } 
-        public string? TrangThai { get; set; } 
+        
     }
 }
