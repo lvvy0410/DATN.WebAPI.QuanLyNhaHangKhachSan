323ef2fcbc8e0f17f582a6c568a95dd6a2016af2
thanhlen2502 <113028186+subalame11@users.noreply.github.com>
2023-06-28 18:54:32 +0700

    phiếu thu
    phiếu thu

--- DAO/PhieuThuDAO.cs
+++ DAO/PhieuThuDAO.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using DTO.Context;
+using DTO.Public;
+using DTO.publicDTO;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +12,48 @@ namespace DAO
 {
     public class PhieuThuDAO
     {
+        private readonly QuanLyNhaHangKhachSanContext dbcontext;
+        public PhieuThuDAO(QuanLyNhaHangKhachSanContext dbcontext)
+        {
+            this.dbcontext = dbcontext;
+        }
+        public async Task<ErrorMessageDTO> LayDanhSachPhieuThu()
+        {
+            ErrorMessageDTO error = new ErrorMessageDTO();
+            try
+            {
+                error.data=await dbcontext.PhieuThus.ToListAsync();
+               error.flagThanhCong= true;
+                return await Task.FromResult(error);
+            }
+            catch (Exception ex)
+            {
+
+                error.flagBiLoiEx = true;
+                error.errorCode = Convert.ToInt32(ErrorCodeEnum.InternalServerError).ToString();
+                error.message = ex.Message;
+                return await Task.FromResult(error);
+            }
+        }
+        public async  Task<ErrorMessageDTO> ThemPhieuThu(PhieuThuDTO pt )
+        {
+            ErrorMessageDTO error = new ErrorMessageDTO();
+            try
+            {
+                error.flagThanhCong= true;
+                dbcontext.PhieuThus.Add(pt);
+                await dbcontext.SaveChangesAsync();
+                return await Task.FromResult(error);
+                
+            }
+            catch(Exception ex)
+            {
+                error.flagBiLoiEx = true;
+                error.errorCode = Convert.ToInt32(ErrorCodeEnum.InternalServerError).ToString();
+                error.message = ex.Message;
+                return await Task.FromResult(error);
+            }
+
+        }
     }
 }
---
+++ DATN.WebAPI.QuanLyNhaHangKhachSan/Controllers/PhieuThuController.cs
@@ -0,0 +1,83 @@
+﻿using DAO;
+using DTO.Context;
+using DTO.Model;
+using DTO.Public;
+using DTO.publicDTO;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Net;
+
+namespace DATN.WebAPI.QuanLyNhaHangKhachSan.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PhieuThuController : ControllerBase
+    {
+        private readonly QuanLyNhaHangKhachSanContext dbcontext;
+        private readonly PhieuThuDAO phieuThuDAO;
+        
+        public PhieuThuController(QuanLyNhaHangKhachSanContext dbcontext, PhieuThuDAO phieuThuDAO)
+        {
+            this.dbcontext = dbcontext;
+            this.phieuThuDAO = phieuThuDAO;
+        }
+        [HttpPost]
+        [Route("danhsach_phieuthu")]
+        public async Task<ActionResult<ResponseDTO>> DanhSachPT()
+        {
+            ResponseDTO responseDTO = new ResponseDTO();
+            try
+            {
+                ErrorMessageDTO error = await phieuThuDAO.LayDanhSachPhieuThu();
+                if(error.flagBiLoiEx|| !error.flagThanhCong)
+                {
+                    responseDTO.errorCode=error.errorCode;
+                    responseDTO.message=error.message;
+                    return Ok(responseDTO);
+                }
+                responseDTO.statusCode = HttpStatusCode.OK;
+                responseDTO.message=HttpStatusCode.OK.ToString();
+                responseDTO.data = error.data;
+                return Ok(responseDTO);
+            }
+            catch (Exception ex)
+            {
+
+                responseDTO.statusCode = HttpStatusCode.BadRequest;
+                responseDTO.errorCode = Convert.ToInt32(ErrorCodeEnum.BadRequest).ToString();
+                responseDTO.message = ex.Message;
+                return BadRequest(responseDTO);
+            }
+        }
+        [HttpPost]
+        [Route("them_phieuthu")]
+        public async Task<ActionResult<PhieuThu>> ThemPhieuThu(PhieuThuDTO pt)
+        {
+           ResponseDTO responseDTO = new ResponseDTO();
+            try
+            {
+                ErrorMessageDTO error = await phieuThuDAO.ThemPhieuThu(pt);
+                    if(error.flagBiLoiEx)
+                {
+                    responseDTO.errorCode=Convert.ToInt32(ErrorCodeEnum.KhongTheThem).ToString();
+                    responseDTO.message = ResponseDTO.GetValueError(ErrorCodeEnum.KhongTheThem);
+                    return Ok(responseDTO);
+                }
+                    responseDTO.statusCode = HttpStatusCode.OK;
+                responseDTO.errorCode = Convert.ToInt32(ErrorCodeEnum.ThemThanhCong).ToString();
+                responseDTO.message = ResponseDTO.GetValueError(ErrorCodeEnum.ThemThanhCong);
+                return Ok(responseDTO);
+            }catch(Exception ex)
+            {
+
+                responseDTO.statusCode = HttpStatusCode.BadRequest;
+                responseDTO.errorCode = Convert.ToInt32(ErrorCodeEnum.BadRequest).ToString();
+                responseDTO.message = ex.Message;
+                return BadRequest(responseDTO);
+            }
+        }
+      
+    }
+}
